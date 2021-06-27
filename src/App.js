3671393a8212933a9ccc2e310249fa52d1055acc
seilyn <seilyn_@naver.com>
2021-06-28 04:27:16 +0900

    https://github.com/seilyn/movie_app_2021.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import PropTypes from "prop-types"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  React.Component { // Dont have a return , has a render method
+  state = { //data is change
+    count: 0
+  };
+
+  add = () => { // add() : 즉시, add : 클릭할 때만
+    console.log("add");
+    this.setState(current => ({ count: current.count +1 }));
+  }
+  minus = () => {
+    console.log("minus");
+    this.setState(current => ({ count: current.count -1 }));
+  }
+
+  render (){
+    return (
+      <div>
+        <h1>The number is : {this.state.count} </h1>
+        <button onClick = {this.add}> Add </button> 
+        <button>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
